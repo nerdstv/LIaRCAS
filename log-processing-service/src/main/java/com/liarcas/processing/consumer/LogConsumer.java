09ae4a16d51d86ad06bebc635275b23aa7b5a65a
Satvik <satvikshrivastava12gmail.com>
2026-04-21 17:43:56 +0530

    Remove groupId from KafkaListener in LogConsumer

--- log-processing-service/src/main/java/com/liarcas/processing/consumer/LogConsumer.java
+++ log-processing-service/src/main/java/com/liarcas/processing/consumer/LogConsumer.java
@@ -7,7 +7,7 @@ import com.liarcas.models.LogEvent;
 
 @Component
 public class LogConsumer {
-    @KafkaListener(topics = "raw-logs", groupId = "log-processing-group")
+    @KafkaListener(topics = "raw-logs")
     public void consume(LogEvent message) {   
         System.out.println("Consumed log event: " + message);
     }
